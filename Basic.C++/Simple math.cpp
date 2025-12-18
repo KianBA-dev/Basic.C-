edd61e76f9d56864e25b786eadc6a4444a386d9c
KianBA-dev <kba.code.engineer@gmail.com>
2025-12-18 19:57:05 +0330

    New code

---
+++ Basic.C++/Simple math.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+	int num1, num2, sum;
+	std::cout << "Please enter two numbers: ";
+		std::cin >> num1;
+		std::cin >> num2;
+		sum = num1 + num2;
+			std::cout << sum;
+			return 0;
+}
